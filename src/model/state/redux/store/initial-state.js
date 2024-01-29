3b081d0f5d7fb5a90d34ee0d2a60f25d81b37527
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-01-29 17:33:06 +0000

    Updating initial-state.js

--- src/model/state/redux/store/initial-state.js
+++ src/model/state/redux/store/initial-state.js
@@ -2,6 +2,6 @@ import {Map} from '@/model/helper/extendable-immutable/map'
 
 let initialState = new Map()
 
-initialState.setIn('external.api.status', new Map())
+initialState.setIn('api.funttastic.client.status', new Map())
 
 export {initialState}
