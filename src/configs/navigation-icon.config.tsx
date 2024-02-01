864f6a1f524f7f827128840e77dbe3e375c43c08
Hudson Ferraz Oliveira <hudson.oliveira@funttastic.com>
2024-02-01 10:24:48 -0300

    added link icons and files page

--- src/configs/navigation-icon.config.tsx
+++ src/configs/navigation-icon.config.tsx
@@ -5,11 +5,23 @@ import {
     HiOutlineViewGridAdd,
     HiOutlineHome,
 } from 'react-icons/hi'
+import fun from '../../public/img/logo/logo-funttastic-transparent.png'
+import kuji from '../../public/img/logo/kuji.svg'
+import hummingbot from '../../public/img/logo/hummingbot-logo.svg'
+import filebrowser from '../../public/img/logo/filebrowser-logo.svg'
 
 export type NavigationIcons = Record<string, JSX.Element>
 
 const navigationIcon: NavigationIcons = {
     home: <HiOutlineHome />,
+    kujira: <img src={kuji} alt="kujira" width={25} height={25} />,
+    fun: <img src={fun} alt="fun" width={25} height={25} />,
+    hummingbot: (
+        <img src={hummingbot} alt="hummingbot" width={25} height={25} />
+    ),
+    filebrowser: (
+        <img src={filebrowser} alt="filebrowser" width={25} height={25} />
+    ),
     singleMenu: <HiOutlineViewGridAdd />,
     collapseMenu: <HiOutlineTemplate />,
     groupSingleMenu: <HiOutlineDesktopComputer />,
