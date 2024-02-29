c0649b56c52fc747cdadc917d3131761f6b50eca
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-02-29 18:27:40 +0000

    New code to call the Fun Client API.

--- src/model/service/api/funttastic.ts
+++ src/model/service/api/funttastic.ts
@@ -1,21 +1,58 @@
-export const apiPostAddWallet = async (password: string) => {
-    const apiUrl = '/wallet/add'
+import axios, {AxiosRequestConfig} from 'axios';
+import fs from 'fs';
 
-    const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            password: password,
-        }),
-    })
+interface ExtraOptions {
+    protocol?: 'http' | 'https';
+    host?: string;
+    port?: number;
+    prefix?: string;
+    clientCertificatePath?: string;
+    clientKeyPath?: string;
+    bearerToken?: string;
+}
 
-    if (!response.ok) {
-        throw new Error(`Error: ${response.status}`)
+interface Options extends AxiosRequestConfig, ExtraOptions {}
+
+async function callAPI(options: Options): Promise<any> {
+    try {
+        if (!options.protocol) options.protocol = "https"
+        if (!options.host) options.host = "localhost"
+        if (!options.port) options.port = import.meta.env.VITE_FUN_CLIENT_PORT || '50001';
+        if (!options.baseURL) options.baseURL = `${options.protocol}://${options.host}:${options.port}${options.prefix}`;
+        if (!options.headers) options.headers = { 'Content-Type': 'application/json' }
+
+        if (options.clientCertificatePath && options.clientKeyPath) {
+            options.httpsAgent = new axios.HttpsAgent({
+                cert: fs.readFileSync(options.clientCertificatePath),
+                key: fs.readFileSync(options.clientKeyPath),
+            });
+        }
+
+        if (options.bearerToken) {
+            options.headers["Authorization"] = `Bearer ${options.bearerToken}`;
+        }
+
+        const response = await axios(options);
+
+        if (response.status < 200 || response.status > 299) {
+            throw new Error(`Request failed. Status ${response.status}. Response: ${response.data}`);
+        }
+
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            throw new Error(`Axios error: ${error.message}`);
+        } else {
+            throw error;
+        }
     }
+}
 
-    const data = await response.json()
+export const apiPostAddWallet = async (password: string) => {
+    return await callAPI({
+        method: "POST",
+        url: "/wallet/add",
+        data: { password }
 
-    return data
+    })
 }
