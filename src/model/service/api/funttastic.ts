28a33726d6f4866383ed2954212574f05271dfc1
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-03-01 18:56:14 +0000

    Adding new API endpoints.

--- src/model/service/api/funttastic.ts
+++ src/model/service/api/funttastic.ts
@@ -56,10 +56,115 @@ async function callAPI(options: Options): Promise<any> {
     }
 }
 
-export const apiPostAddWallet = async (password: string) => {
+
+export const apiPostAuthLogin = async (username: string, password: string) => {
+    return callAPI({
+        method: "POST",
+        url: "/auth/login",
+        data: { username, password },
+    });
+}
+
+export const apiGetAuthToken = async () => {
+    return callAPI({
+        method: "GET",
+        url: "/auth/token",
+    });
+}
+
+export const apiGetServiceStatus = async (id?: string) => {
+    return callAPI({
+        method: "GET",
+        url: "/service/status",
+        data: { id }
+    });
+}
+
+export const apiPostServiceStart = async (id?: string) => {
+    return callAPI({
+        method: "POST",
+        url: "/service/start",
+        data: { id }
+    });
+}
+
+export const apiPostServiceStop = async (id?: string) => {
+    return callAPI({
+        method: "POST",
+        url: "/service/stop",
+        data: { id }
+    });
+}
+
+export const apiGetStrategyStatus = async (strategy?: string, version?: string, id?: string) => {
+    return callAPI({
+        method: "GET",
+        url: "/strategy/status",
+        data: { strategy, version, id }
+    });
+}
+
+export const apiPostStrategyStart = async (strategy?: string, version?: string, id?: string) => {
+    return callAPI({
+        method: "POST",
+        url: "/strategy/start",
+        data: { strategy, version, id }
+    });
+}
+
+export const apiPostStrategyStop = async (strategy?: string, version?: string, id?: string) => {
+    return callAPI({
+        method: "POST",
+        url: "/strategy/stop",
+        data: { strategy, version, id }
+    });
+}
+
+export const apiGetStrategyWorkerStatus = async (strategy?: string, version?: string, id?: string, workerId?: string) => {
+    return callAPI({
+        method: "GET",
+        url: "/strategy/worker/status",
+        data: { strategy, version, id, workerId }
+    });
+}
+
+export const apiPostStrategyWorkerStart = async (strategy?: string, version?: string, id?: string, workerId?: string) => {
+    return callAPI({
+        method: "POST",
+        url: "/strategy/worker/start",
+        data: { strategy, version, id, workerId }
+    });
+}
+
+export const apiPostStrategyWorkerStop = async (strategy?: string, version?: string, id?: string, workerId?: string) => {
+    return callAPI({
+        method: "POST",
+        url: "/strategy/worker/stop",
+        data: { strategy, version, id, workerId }
+    });
+}
+
+export const apiPostAddWallet = async (privateKey: string) => {
     return callAPI({
         method: "POST",
         url: "/wallet/add",
-        data: { password },
+        data: {
+            chain: "kujira",
+            network: "testnet",
+            connector: "kujira",
+            privateKey: privateKey,
+            accountNumber: 0
+        },
+    });
+}
+
+export const apiDeleteRemoveWallet = async (publicKey: string) => {
+    return callAPI({
+        method: "DELETE",
+        url: "/wallet/remove",
+        data: {
+            "chain": "kujira",
+            "address": publicKey
+        },
     });
 }
