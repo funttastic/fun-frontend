ba23eaf742f078df7e320a9e924221778d2aa8fc
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-03-18 20:07:35 +0000

    Improving TermsAndConditions.tsx component.

--- src/views/TermsAndConditions.tsx
+++ src/views/TermsAndConditions.tsx
@@ -1,21 +1,35 @@
 export default function TermsAndConditions() {
   return (
-    <div>
-      <h1 className="flex justify-center">Terms & Conditions</h1>
-      <div className="mt-5 text-lg">
-        <p>
-          By accessing and using this service, you acknowledge and agree that the use of the service is at your own risk. The company, its affiliates, and its licensors shall not be liable for any direct, indirect, incidental, consequential, special, or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses (even if the company has been advised of the possibility of such damages), resulting from: (i) the use or the inability to use the service; (ii) the cost of procurement of substitute goods and services resulting from any goods, data, information or services purchased or obtained or messages received or transactions entered into through or from the service; (iii) unauthorized access to or alteration of your transmissions or data; (iv) statements or conduct of any third party on the service; or (v) any other matter relating to the service.
+    <div className="px-8 py-4">
+      <h1 className="text-3xl font-bold text-center my-5">Terms & Conditions</h1>
+      <div className="text-sm leading-loose">
+        <p className="mb-4">
+          By accessing and using this service, you acknowledge and agree that the use of the service is at your own risk. The company, its affiliates, and its licensors shall not be liable for any:
         </p>
-        <p>
-          The company does not warrant that: (i) the service will meet your specific requirements, (ii) the service will be uninterrupted, timely, secure, or error-free, (iii) the results that may be obtained from the use of the service will be accurate or reliable, (iv) the quality of any products, services, information, or other material purchased or obtained by you through the service will meet your expectations, and (v) any errors in the service will be corrected.
+        <ol className="list-decimal list-inside mb-4">
+          <li>Direct, indirect, incidental, consequential, special, or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses (even if the company has been advised of the possibility of such damages), resulting from the use or the inability to use the service;</li>
+          <li>The cost of procurement of substitute goods and services resulting from any goods, data, information or services purchased or obtained or messages received or transactions entered into through or from the service;</li>
+          <li>Unauthorized access to or alteration of your transmissions or data;</li>
+          <li>Statements or conduct of any third party on the service;</li>
+          <li>Any other matter relating to the service.</li>
+        </ol>
+        <p className="mb-4">
+          The company does not warrant that:
         </p>
-        <p>
+        <ol className="list-decimal list-inside mb-4">
+          <li>The service will meet your specific requirements;</li>
+          <li>The service will be uninterrupted, timely, secure, or error-free;</li>
+          <li>The results that may be obtained from the use of the service will be accurate or reliable;</li>
+          <li>The quality of any products, services, information, or other material purchased or obtained by you through the service will meet your expectations;</li>
+          <li>Any errors in the service will be corrected.</li>
+        </ol>
+        <p className="mb-4">
           Any material downloaded or otherwise obtained through the use of the service is done at your own discretion and risk and you will be solely responsible for any damage to your computer system or loss of data that results from the download of any such material.
         </p>
-        <p>
+        <p className="mb-4">
           No advice or information, whether oral or written, obtained by you from the company or through or from the service shall create any warranty not expressly stated in the terms.
         </p>
-        <p>
+        <p className="mb-4">
           The company's aggregate liability to you for any and all claims arising from the use of the service is limited to the amount you paid the company, if any, for access to and use of the service.
         </p>
       </div>
