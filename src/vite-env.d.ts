3766074abf5c309b3ec525c4c05bf45d92a3191c
guilhermebarbosaantunes <guilherme-barbosa-funttastic>
2024-05-13 22:51:15 -0300

    environment variable.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,14 @@
 /// <reference types="vite/client" />
 
 
-interface ImportMetaEnv {
-  readonly VITE_WEBSOCKET_PORT?: string;
+  interface ImportMetaEnv {
+    VITE_WEBSOCKET_PORT?: string;
+    VITE_SECRET_PORT?: number;
+    VITE_PASSWORD?: string;
+    VITE_HOST?: string;
 
-}
+  }
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
