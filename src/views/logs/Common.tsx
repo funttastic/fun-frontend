85330f1f0404e0adf1d4ae37babb1143930ee72f
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-05-27 11:34:29 -0300

    scroll the logs page.

--- src/views/logs/Common.tsx
+++ src/views/logs/Common.tsx
@@ -11,6 +11,7 @@ const Common: React.FC<CommonProps> = ({ id, name }) => {
 
   return (
     <Card
+      className="flex flex-col py-8"
       header={<div className="text-center font-bold py-3 text-gray-100"><h4>{name}</h4></div>}
     >
       <div>
