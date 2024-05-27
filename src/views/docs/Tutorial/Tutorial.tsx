b64d387283d9036f704e325bb7281f58d265f2d9
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-05-27 16:38:13 -0300

    width page.

--- src/views/docs/Tutorial/Tutorial.tsx
+++ src/views/docs/Tutorial/Tutorial.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Tutorial() {
   return (
-    <div className="size-full overflow-hidden border-none no-scrollbar">
+    <div className="size-full overflow-hidden border-none no-scrollbar flex flex-col py-6">
       <iframe
         title="Kujira Connector for Hummingbot (Video Demonstration)"
         src={"https://funttastic.github.io/static/partners/kujira/bot/index.html"}
