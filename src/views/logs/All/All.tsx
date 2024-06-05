aae6249d0d94439e8ee6007da05dd40ddeb56cf0
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-06-05 19:10:11 -0300

    Updating title.

--- src/views/logs/All/All.tsx
+++ src/views/logs/All/All.tsx
@@ -1,7 +1,7 @@
 import Common from "@/views/logs/Common";
 
 const AllAll = () => {
-  return <Common id={'all.all'} name={"All All Logs"}/>
+  return <Common id={'all.all'} name={"All Logs"}/>
 }
 
 export default AllAll
