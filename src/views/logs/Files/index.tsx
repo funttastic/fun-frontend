a2a68df8fa575af4d198bc8b2d67912721da8071
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-06-10 23:15:47 -0300

    adding nem feature "wizard".

--- src/views/logs/Files/index.tsx
+++ src/views/logs/Files/index.tsx
@@ -1,3 +1,4 @@
+import Wizard from './Wizard';
 import IFrame from '@/components/utils/IFrame'
 
 const filebrowserPort = import.meta.env.VITE_FILEBROWSER_PORT || '50002';
@@ -9,6 +10,7 @@ export default function Files() {
                 title={'Funttastic'}
                 src={`http://localhost:${filebrowserPort}/files/`}
             ></IFrame>
+          <Wizard/>
         </div>
     )
 }
