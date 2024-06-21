160514d111faab0476a93f2e6f5ac610b18b6607
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-06-21 18:05:50 -0300

    Wizard style.

--- src/views/Files.tsx
+++ src/views/Files.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import IFrame from '@/components/utils/IFrame';
 import './wizard/wizard.css';
-import Wizard from './wizard/wizard';
 
 const filebrowserPort = import.meta.env.VITE_FILEBROWSER_PORT || '50002';
 
 const Files: React.FC = () => {
   return (
     <div className="size-full overflow-hidden border-none no-scrollbar">
-      {/*<IFrame*/}
-      {/*  title={'Funttastic'}*/}
-      {/*  src={`http://localhost:${filebrowserPort}/files/`}*/}
-      {/*/>*/}
-      <Wizard />
+      <IFrame
+        title={'Funttastic'}
+        src={`http://localhost:${filebrowserPort}/files/`}
+      />
     </div>
   );
 }
--- src/views/wizard/wizard.tsx
+++ src/views/wizard/wizard.tsx
@@ -10,7 +10,7 @@ import StepOne from "@/views/wizard/stepOne";
 import StepTwo from "@/views/wizard/stepTwo";
 import StepTree from "@/views/wizard/stepTree";
 
-const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+const steps = ['Enter your Mnemonic', 'Create an ad group', 'Create an ad'];
 
 interface StepComponentRef {
   validateStep: () => Promise<boolean> | boolean;
@@ -68,64 +68,66 @@ export default function Wizard() {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{width: '100%'}}>
       <Stepper activeStep={activeStep}>
         {steps.map((label, index) => {
           const stepProps: { completed?: boolean } = {};
           const labelProps: {
             optional?: React.ReactNode;
           } = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
+
           if (isStepSkipped(index)) {
             stepProps.completed = false;
           }
+
           return (
             <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
+              <StepLabel {...labelProps}>
+                <Box
+                  sx={index === activeStep ? {color: 'white'} : {}}>
+                  {label}
+                </Box>
+              </StepLabel>
             </Step>
           );
         })}
       </Stepper>
       {activeStep === steps.length ? (
         <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
+          <Typography sx={{mt: 2, mb: 1}}>
             All steps completed - you&apos;re finished
           </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
+          <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
+            <Box sx={{flex: '1 1 auto'}}/>
             <Button onClick={handleReset}>Reset</Button>
           </Box>
         </React.Fragment>
       ) : (
         <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+          <Typography sx={{mt: 2, mb: 1}}>Step {activeStep + 1}</Typography>
 
-          <Box sx={{ mt: 2, mb: 1 }}>
-            {activeStep === 0 && <StepOne ref={(el: StepComponentRef | null) => stepRefs.current[0] = el} />}
-            {activeStep === 1 && <StepTwo ref={(el: StepComponentRef | null) => stepRefs.current[1] = el} />}
-            {activeStep === 2 && <StepTree ref={(el: StepComponentRef | null) => stepRefs.current[2] = el} />}
+          <Box sx={{mt: 2, mb: 1}}>
+            {activeStep === 0 && <StepOne ref={(el: StepComponentRef | null) => stepRefs.current[0] = el}/>}
+            {activeStep === 1 && <StepTwo ref={(el: StepComponentRef | null) => stepRefs.current[1] = el}/>}
+            {activeStep === 2 && <StepTree ref={(el: StepComponentRef | null) => stepRefs.current[2] = el}/>}
           </Box>
 
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+          <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
             <Button
               color="inherit"
               disabled={activeStep === 0}
               onClick={handleBack}
-              sx={{ mr: 1 }}
+              sx={{mr: 1}}
             >
               Back
             </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
+            <Box sx={{flex: '1 1 auto'}}/>
             {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+              <Button color="inherit" onClick={handleSkip} sx={{mr: 1}}>
                 Skip
               </Button>
             )}
-            <Button onClick={handleNext}>
+            <Button color="inherit" onClick={handleNext}>
               {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
             </Button>
           </Box>
