d37a7b7d303b5ceae7d944ccf4affa7f1ad8b8bc
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-07-10 19:02:52 -0300

    creating market form.

--- src/views/wizard/stepSix.tsx
+++ src/views/wizard/stepSix.tsx
@@ -1,88 +1,89 @@
-import './wizard.css';
-import * as Yup from 'yup';
-import React, { forwardRef, useImperativeHandle } from 'react';
-import { useForm, Controller, FieldErrors, Control } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { ValidationError } from 'yup';
+import React from 'react';
+import Box from "@mui/material/Box";
 
-
-interface FormValues {
-  layer: string;
-}
-
-interface StepComponentRef {
-  validateStep: () => Promise<boolean>;
-}
-
-interface StepProps {
-  control: Control;
-  errors: FieldErrors;
-  handleNext: () => Promise<void>;
-  handleBack: () => void;
-}
-
-const sanitizeLayer = (layer: string) => {
-  return layer.replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s+/g, ' ').trim();
-};
-
-const layerValidationSchema = Yup.object({
-  layer: Yup.string()
-    .required('layer is required')
-});
-
-type StepComponentProps = StepProps & React.RefAttributes<StepComponentRef>;
-
-const StepSix = forwardRef<StepComponentRef, StepComponentProps>(
-  (_props, ref) => {
-    const {control, handleSubmit, formState: {errors}, getValues, setError, setValue} = useForm({
-      resolver: yupResolver(layerValidationSchema),
-    });
-
-    useImperativeHandle(ref, () => ({
-      validateStep: async () => {
-        let values = getValues();
-        values.layer = sanitizeLayer(values.layer);
-        setValue('layer', values.layer);
-        try {
-          await layerValidationSchema.validate(values, { abortEarly: false });
-          return true;
-        } catch (error) {
-          if (error instanceof ValidationError && error.inner) {
-            error.inner.forEach(validationError => {
-              setError(validationError.path as keyof typeof values, { message: validationError.message });
-            });
-          }
-          return false;
+const data = {
+  id: "01",
+  wallet: "kujira10uaufamh5lethtxl2l4sunelyngedsy054klf6",
+  market: "KUJI/USK",
+  strategy: {
+    layers: {
+      bid: {
+        quantity: 2,
+        spread: {
+          absolute: null,
+          percentage: 15
+        },
+        budget: {
+          absolute: 1,
+          percentage: 0.1
+        }
+      },
+      ask: {
+        quantity: 1,
+        spread: {
+          absolute: null,
+          percentage: 15
+        },
+        budget: {
+          absolute: 1,
+          percentage: 0.1
         }
       }
-    }));
-
-    const onSubmit = (data: FormValues) => {
-      data.layer = sanitizeLayer(data.layer);
-      console.log(data);
-    };
-
-    return (
-      <form className="wizard" onSubmit={handleSubmit(onSubmit)}>
-        <div className="step">
-          <h4>Enter your layer and Crypto</h4>
-          <div className="field">
-            <Controller
-              name="layer"
-              control={control}
-              render={({ field }) => <input className="input-Five" type="text" {...field} />}
-            />
-            {errors.layer && <div className="error-message">{errors.layer.message}</div>}
-          </div>
-          <div className="text-exp">
-            <p>
-
-            </p>
-          </div>
-        </div>
-      </form>
-    );
+    }
   }
+};
+
+const renderInput = (label: string, value: string | number | null) => (
+  <div style={{ marginBottom: '10px', color:'black' }}>
+    <label>
+      {label}:
+      <input style={{ borderRadius:'3px'}} type="text" defaultValue={value === null ? '' : value.toString()} />
+    </label>
+  </div>
 );
 
-export default StepSix;
+const MyComponent: React.FC = () => {
+  return (
+    <Box className="container">
+     <div className="container-text">
+        {renderInput('id', data.id)}
+        {renderInput('wallet', data.wallet)}
+        {renderInput('market', data.market)}
+        <div>
+          <p>strategy:</p>
+          <ul>
+            <li>layers:</li>
+            <ul>
+              <li>bid:</li>
+              {renderInput('quantity', data.strategy.layers.bid.quantity)}
+              <li>spread:</li>
+              <ul>
+                {renderInput('absolute', data.strategy.layers.bid.spread.absolute)}
+                {renderInput('percentage', data.strategy.layers.bid.spread.percentage)}
+              </ul>
+              <li>budget:</li>
+              <ul>
+                {renderInput('absolute', data.strategy.layers.bid.budget.absolute)}
+                {renderInput('percentage', data.strategy.layers.bid.budget.percentage)}
+              </ul>
+              <li>ask:</li>
+              {renderInput('quantity', data.strategy.layers.ask.quantity)}
+              <li>spread:</li>
+              <ul>
+                {renderInput('absolute', data.strategy.layers.ask.spread.absolute)}
+                {renderInput('percentage', data.strategy.layers.ask.spread.percentage)}
+              </ul>
+              <li>budget:</li>
+              <ul>
+                {renderInput('absolute', data.strategy.layers.ask.budget.absolute)}
+                {renderInput('percentage', data.strategy.layers.ask.budget.percentage)}
+              </ul>
+            </ul>
+          </ul>
+        </div>
+     </div>
+    </Box>
+  );
+};
+
+export default MyComponent;
