a6e22be40f80fae84f8f99a75f4ae308e5cefcd4
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-07-12 16:02:19 -0300

    adding text editor.

--- src/views/wizard/stepSix.tsx
+++ src/views/wizard/stepSix.tsx
@@ -1,89 +1,65 @@
-import React from 'react';
-import Box from "@mui/material/Box";
+import React, { useState } from "react";
+import AceEditor from "react-ace";
+import yaml from "js-yaml";
 
-const data = {
-  id: "01",
-  wallet: "kujira10uaufamh5lethtxl2l4sunelyngedsy054klf6",
-  market: "KUJI/USK",
-  strategy: {
-    layers: {
-      bid: {
-        quantity: 2,
-        spread: {
-          absolute: null,
-          percentage: 15
-        },
-        budget: {
-          absolute: 1,
-          percentage: 0.1
-        }
-      },
-      ask: {
-        quantity: 1,
-        spread: {
-          absolute: null,
-          percentage: 15
-        },
-        budget: {
-          absolute: 1,
-          percentage: 0.1
-        }
+
+import "ace-builds/src-noconflict/mode-yaml";
+import "ace-builds/src-noconflict/theme-github";
+
+const initialYaml = `
+layers:
+  - bid:
+      quantity: 2
+      spread:
+        absolute: null
+        percentage: 15
+      budget:
+        absolute: 1 # in USD
+        percentage: 0.1
+    ask:
+      quantity:   1
+      spread:
+        absolute: null
+        percentage: 15
+      budget:
+        absolute: 1 # in USD
+        percentage: 0.1
+`;
+
+
+const YamlEditor: React.FC = () => {
+  const [yamlContent, setYamlContent] = useState<string>(initialYaml);
+  const [parsedContent, setParsedContent] = useState<object | null>(null);
+
+  const handleYamlChange = (newContent: string) => {
+    setYamlContent(newContent);
+    try {
+      const parsed = yaml.load(newContent);
+      if (typeof parsed === "object" && parsed !== null) {
+        setParsedContent(parsed);
+      } else {
+        setParsedContent(null);
       }
+    } catch (e) {
+      setParsedContent(null);
+      console.error("Error parsing YAML:", e);
     }
-  }
-};
+  };
 
-const renderInput = (label: string, value: string | number | null) => (
-  <div style={{ marginBottom: '10px', color:'black' }}>
-    <label>
-      {label}:
-      <input style={{ borderRadius:'3px'}} type="text" defaultValue={value === null ? '' : value.toString()} />
-    </label>
-  </div>
-);
-
-const MyComponent: React.FC = () => {
   return (
-    <Box className="container">
-     <div className="container-text">
-        {renderInput('id', data.id)}
-        {renderInput('wallet', data.wallet)}
-        {renderInput('market', data.market)}
-        <div>
-          <p>strategy:</p>
-          <ul>
-            <li>layers:</li>
-            <ul>
-              <li>bid:</li>
-              {renderInput('quantity', data.strategy.layers.bid.quantity)}
-              <li>spread:</li>
-              <ul>
-                {renderInput('absolute', data.strategy.layers.bid.spread.absolute)}
-                {renderInput('percentage', data.strategy.layers.bid.spread.percentage)}
-              </ul>
-              <li>budget:</li>
-              <ul>
-                {renderInput('absolute', data.strategy.layers.bid.budget.absolute)}
-                {renderInput('percentage', data.strategy.layers.bid.budget.percentage)}
-              </ul>
-              <li>ask:</li>
-              {renderInput('quantity', data.strategy.layers.ask.quantity)}
-              <li>spread:</li>
-              <ul>
-                {renderInput('absolute', data.strategy.layers.ask.spread.absolute)}
-                {renderInput('percentage', data.strategy.layers.ask.spread.percentage)}
-              </ul>
-              <li>budget:</li>
-              <ul>
-                {renderInput('absolute', data.strategy.layers.ask.budget.absolute)}
-                {renderInput('percentage', data.strategy.layers.ask.budget.percentage)}
-              </ul>
-            </ul>
-          </ul>
-        </div>
-     </div>
-    </Box>
+    <div style={{ padding:"50px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <AceEditor
+        mode="yaml"
+        theme="github"
+        name="yaml-editor"
+        value={yamlContent}
+        onChange={handleYamlChange}
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{ useWorker: false }}
+        className="container-text"
+      />
+    </div>
   );
 };
 
-export default MyComponent;
+export default YamlEditor;
