f75fe8b7e9e352d2a8958c2076b3ba34e2847d55
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-07-16 20:08:48 -0300

    adding types/js-yaml, monaco editor.

--- src/views/wizard/stepSix.tsx
+++ src/views/wizard/stepSix.tsx
@@ -1,11 +1,6 @@
-import './wizard.css';
-import yaml from "js-yaml";
-import AceEditor from "react-ace";
-import React, { useState } from "react";
-
-import "ace-builds/src-noconflict/mode-yaml";
-import "ace-builds/src-noconflict/theme-github";
-import "ace-builds/src-noconflict/ext-language_tools";
+import React, { useState, useRef } from 'react';
+import Editor from '@monaco-editor/react';
+import * as yaml from 'js-yaml';
 
 const initialYaml = `
 layers:
@@ -18,7 +13,7 @@ layers:
         absolute: 1 
         percentage: 0.1
     ask:
-      quantity:   1
+      quantity: 1
       spread:
         absolute: null
         percentage: 15
@@ -27,50 +22,31 @@ layers:
         percentage: 0.1
 `;
 
+const TemplateDemo: React.FC = () => {
+  const [text, setText] = useState(initialYaml);
+  const editorRef = useRef<any>(null);
 
-const YamlEditor: React.FC = () => {
-  const [yamlContent, setYamlContent] = useState<string>(initialYaml);
-  const [parsedContent, setParsedContent] = useState<object | null>(null);
-
-  const handleYamlChange = (newContent: string) => {
-    setYamlContent(newContent);
-    try {
-      const parsed = yaml.load(newContent);
-      if (typeof parsed === "object" && parsed !== null) {
-        setParsedContent(parsed);
-      } else {
-        setParsedContent(null);
-      }
-    } catch (e) {
-      setParsedContent(null);
-      console.error("Error parsing YAML:", e);
-    }
-  };
 
   return (
-    <div style={{ padding:"50px", display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <AceEditor
-        mode="yaml"
-        theme="github"
-        name="yaml-editor"
-        value={yamlContent}
-        onChange={handleYamlChange}
-        editorProps={{
-          $blockScrolling: true,
-
-        }}
-        setOptions={{
-          cursorStart: 1,
-          useWorker: true,
-          showLineNumbers: true,
-          highlightActiveLine: false,
-          cursorStyle: "wide",
-          wrap: true,
-        }}
+    <div className="editor-container">
+      <Editor
         className="editor-text"
+        language="yaml"
+        theme = 'vs-dark'
+        value={text}
+        options={{
+          selectOnLineNumbers: true,
+          roundedSelection: false,
+          readOnly: false,
+          cursorStyle: 'line',
+          automaticLayout: false,
+          formatOnType: true,
+          tabSize: 2,
+          insertSpaces: true,
+        }}
       />
     </div>
   );
 };
 
-export default YamlEditor;
+export default TemplateDemo;
