7f626ebd7e69ffa9b559b74176cff913a2848ab7
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-07-25 12:12:16 -0300

    adjusting title and classes.

--- src/views/wizard/stepSix.tsx
+++ src/views/wizard/stepSix.tsx
@@ -1,8 +1,6 @@
-import React, {useState, useRef} from 'react';
+import React, {useState} from 'react';
 import Editor from '@monaco-editor/react';
 import './wizard.css';
-import {color} from "framer-motion";
-
 
 const initialYaml = `
 layers:
@@ -26,12 +24,11 @@ layers:
 
 const TemplateDemo: React.FC = () => {
   const [text, setText] = useState(initialYaml);
-  const editorRef = useRef<any>(null);
-
 
   return (
     <>
       <div className="editor-container">
+        <h4 className="editor-header"> Strategy Layer(s) </h4>
         <Editor
           className="editor-text"
           language="yaml"
@@ -61,10 +58,13 @@ const TemplateDemo: React.FC = () => {
           bid (buy) price and the ask (sell) price. It can be configured as an absolute value or a percentage.<br/>
           <strong style={{color: '#F2845C'}}>Absolute</strong> <strong>:</strong> null The absolute spread is not set
           (null), which means the configuration is using the percentage spread.<br/>
-          <strong style={{color: '#F2845C'}}>Percentage</strong> <strong>:</strong>  The percentage spread is set to 15%. This means the buy order will be placed 15% below the current market
+          <strong style={{color: '#F2845C'}}>Percentage</strong> <strong>:</strong> The percentage spread is set to 15%.
+          This means the buy order will be placed 15% below the current market
           price.<br/>
-         <strong style={{color: '#F2845C'}}>Budget</strong>  <strong>:</strong>  The budget is the amount of money you are willing to spend on the buy orders.<br/>
-         <strong style={{color: '#F2845C'}}>Ask</strong> <strong>:</strong>  Represents a sell order. Sell orders are offers to sell an asset at a specific price.
+          <strong style={{color: '#F2845C'}}>Budget</strong> <strong>:</strong> The budget is the amount of money you
+          are willing to spend on the buy orders.<br/>
+          <strong style={{color: '#F2845C'}}>Ask</strong> <strong>:</strong> Represents a sell order. Sell orders are
+          offers to sell an asset at a specific price.
         </p>
       </div>
     </>
