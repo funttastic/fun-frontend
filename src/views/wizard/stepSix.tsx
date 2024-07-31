b5c690c32468bf3546a2821076d147e9864ce7bf
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-07-31 17:59:41 -0300

    validation adjusted and adding typing.

--- src/views/wizard/stepSix.tsx
+++ src/views/wizard/stepSix.tsx
@@ -1,9 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState, forwardRef, useImperativeHandle } from 'react';
 import Editor from '@monaco-editor/react';
 import './wizard.css';
 
-const initialYaml = `
-layers:
+const initialYaml = `layers:
   - bid:
       quantity: 2
       spread:
@@ -22,18 +21,33 @@ layers:
         percentage: 0.1
 `;
 
-const TemplateDemo: React.FC = () => {
+type StepSixProps = {
+  setFormData: (data: any) => void;
+  formData: any;
+};
+
+const StepSix = forwardRef<unknown, StepSixProps>(({ setFormData, formData}, ref) => {
   const [text, setText] = useState(initialYaml);
 
+  useImperativeHandle(ref, () => ({
+    validateStep: async () => {
+      if (text.trim() === '') {
+        return false;
+      }
+      setFormData({ ...formData, strategyLayers: text });
+      return true;
+    }
+  }));
   return (
-    <>
+    <div>
       <div className="editor-container">
         <h4 className="editor-header"> Strategy Layer(s) </h4>
         <Editor
           className="editor-text"
           language="yaml"
-          theme='vs-dark'
+          theme="vs-dark"
           value={text}
+          onChange={value => setText(value || '')}
           options={{
             selectOnLineNumbers: true,
             roundedSelection: false,
@@ -48,26 +62,18 @@ const TemplateDemo: React.FC = () => {
       </div>
       <div className="text-layer">
         <p>
-          <strong style={{color: '#F2845C'}}>Layers</strong> <strong>:</strong> Indicates that there are multiple layers
-          or levels of trading orders. Each layer can represent a different strategy or configuration.<br/>
-          <strong style={{color: '#F2845C'}}>Bid</strong> <strong>:</strong> Represents a buy order. Buy orders are
-          offers to purchase an asset at a specific price.<br/>
-          <strong style={{color: '#F2845C'}}>Quantity</strong> <strong>:</strong> Specifies the number of units of the
-          asset you want to buy. Here, it is set to buy 2 units.<br/>
-          <strong style={{color: '#F2845C'}}>Spread</strong> <strong>:</strong> The spread is the difference between the
-          bid (buy) price and the ask (sell) price. It can be configured as an absolute value or a percentage.<br/>
-          <strong style={{color: '#F2845C'}}>Absolute</strong> <strong>:</strong> null The absolute spread is not set
-          (null), which means the configuration is using the percentage spread.<br/>
-          <strong style={{color: '#F2845C'}}>Percentage</strong> <strong>:</strong> The percentage spread is set to 15%.
-          This means the buy order will be placed 15% below the current market
-          price.<br/>
-          <strong style={{color: '#F2845C'}}>Budget</strong> <strong>:</strong> The budget is the amount of money you
-          are willing to spend on the buy orders.<br/>
-          <strong style={{color: '#F2845C'}}>Ask</strong> <strong>:</strong> Represents a sell order. Sell orders are
-          offers to sell an asset at a specific price.
+          <strong style={{ color: '#F2845C' }}>Layers</strong>: Indicates that there are multiple layers or levels of trading orders. Each layer can represent a different strategy or configuration.<br />
+          <strong style={{ color: '#F2845C' }}>Bid</strong>: Represents a buy order. Buy orders are offers to purchase an asset at a specific price.<br />
+          <strong style={{ color: '#F2845C' }}>Quantity</strong>: Specifies the number of units of the asset you want to buy. Here, it is set to buy 2 units.<br />
+          <strong style={{ color: '#F2845C' }}>Spread</strong>: The spread is the difference between the bid (buy) price and the ask (sell) price. It can be configured as an absolute value or a percentage.<br />
+          <strong style={{ color: '#F2845C' }}>Absolute</strong>: null The absolute spread is not set (null), which means the configuration is using the percentage spread.<br />
+          <strong style={{ color: '#F2845C' }}>Percentage</strong>: The percentage spread is set to 15%. This means the buy order will be placed 15% below the current market price.<br />
+          <strong style={{ color: '#F2845C' }}>Budget</strong>: The budget is the amount of money you are willing to spend on the buy orders.<br />
+          <strong style={{ color: '#F2845C' }}>Ask</strong>: Represents a sell order. Sell orders are offers to sell an asset at a specific price.
         </p>
       </div>
-    </>
+    </div>
   );
-}
-export default TemplateDemo;
+});
+
+export default StepSix;
