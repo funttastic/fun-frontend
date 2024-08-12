9c1aa2c318d26884e3e5e7c0ae878d35ca09495f
guilherme-barbosa-funttastic <guilherme.barbosa@funttastic.com>
2024-08-12 14:56:08 -0300

    adding dispatch, submit removed and adjusted type.

--- src/views/wizard/step3.tsx
+++ src/views/wizard/step3.tsx
@@ -1,9 +1,10 @@
 import './wizard.css';
 import * as Yup from 'yup';
-import React, {useImperativeHandle} from 'react';
-import {FaEye, FaEyeSlash} from "react-icons/fa";
-import {yupResolver} from '@hookform/resolvers/yup';
-import {useForm, Controller, Control, FieldErrors} from 'react-hook-form';
+import React, { useImperativeHandle } from 'react';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, Controller, Control, FieldErrors } from 'react-hook-form';
+import { dispatch } from '@/model/state/redux/store';
 
 interface StepComponentRef {
   validateStep: () => Promise<boolean> | boolean;
@@ -15,8 +16,8 @@ interface FormValues {
 }
 
 interface StepProps {
-  control: Control;
-  errors: FieldErrors;
+  control: Control<FormValues>;
+  errors: FieldErrors<FormValues>;
   handleNext: () => Promise<void>;
   handleBack: () => void;
   formData: FormValues;
@@ -43,30 +44,32 @@ const telegramValidationSchema = Yup.object({
     .matches(/^[0-9]+$/, 'Chat ID must be numeric')
 });
 
-const StepTree = React.forwardRef<StepComponentRef, StepComponentProps>(({ handleNext, handleBack, setFormData, formData }, ref) => {
+const StepTree = React.forwardRef<StepComponentRef, StepComponentProps>(({ formData }, ref) => {
   const [showPassword, setShowPassword] = React.useState(false);
-  const {control, handleSubmit, getValues, setError, setValue, formState: {errors}} = useForm({
+  const { control, setValue, getValues, setError, formState: { errors } } = useForm<FormValues>({
     resolver: yupResolver(telegramValidationSchema),
-    defaultValues: {
-      token: formData.token,
-      chatID: formData.chatID
-    }
+    defaultValues: formData
   });
 
   useImperativeHandle(ref, () => ({
     validateStep: async () => {
-      let values = getValues();
+      const values = getValues();
       values.token = sanitizeTelegram(values.token);
-      setValue('token', values.token);
       values.chatID = _sanitizeTelegram(values.chatID);
+      setValue('token', values.token);
       setValue('chatID', values.chatID);
+
       try {
-        await telegramValidationSchema.validate(values, {abortEarly: false});
+        await telegramValidationSchema.validate(values, { abortEarly: false });
+        dispatch('app.updateWizard', {
+            telegramToken: values.token,
+            telegramChatID: values.chatID
+        })
         return true;
       } catch (error) {
         if (error instanceof Yup.ValidationError && error.inner) {
           error.inner.forEach(validationError => {
-            setError(validationError.path as keyof typeof values, {message: validationError.message});
+            setError(validationError.path as keyof FormValues, { message: validationError.message });
           });
         }
         return false;
@@ -74,39 +77,31 @@ const StepTree = React.forwardRef<StepComponentRef, StepComponentProps>(({ handl
     }
   }));
 
-  const onSubmit = (data: FormValues) => {
-    data.chatID = _sanitizeTelegram(data.chatID);
-    data.token = sanitizeTelegram(data.token);
-    setFormData(data);
-    handleNext();
-    console.log(data);
-  }
-
   return (
-    <form className="wizard" onSubmit={handleSubmit(onSubmit)}>
+    <form className="wizard">
       <div className="step-tree">
         <div className="field-tree">
           <p className="h4">Enter your Telegram Token</p>
           <Controller
             name="token"
             control={control}
-            render={({field}) => (
-              <div>
-                <input className="input-tree" type={showPassword ? 'text' : 'password'} {...field} />
+            render={({ field }) => (
+              <div style={{ position: 'relative' }}>
+                <input className="input-tree" type={showPassword ? 'text' : 'password'} {...field} value={field.value || ''} />
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
                   style={{
                     position: 'absolute',
-                    right: 420,
-                    top: '32.5%',
+                    right: 10,
+                    top: '50%',
                     transform: 'translateY(-50%)',
                     background: 'none',
                     border: 'none',
                     cursor: 'pointer'
                   }}
                 >
-                  {showPassword ? <FaEyeSlash/> : <FaEye/>}
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
                 </button>
               </div>
             )}
@@ -118,58 +113,37 @@ const StepTree = React.forwardRef<StepComponentRef, StepComponentProps>(({ handl
           <Controller
             name="chatID"
             control={control}
-            render={({field}) => (
-              <div>
-                <input className="input-tree" type={showPassword ? 'text' : 'password'} {...field} />
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  style={{
-                    position: 'absolute',
-                    right: 420,
-                    top: '51.5%',
-                    transform: 'translateY(-50%)',
-                    background: 'none',
-                    border: 'none',
-                    cursor: 'pointer'
-                  }}
-                >
-                  {showPassword ? <FaEyeSlash/> : <FaEye/>}
-                </button>
-              </div>
+            render={({ field }) => (
+              <input className="input-tree" type="text" {...field} {...field} value={field.value || ''}/>
             )}
           />
           {errors.chatID && <div className="error-message-tree">{errors.chatID.message}</div>}
         </div>
         <div className="text-tree">
-
-          The <strong style={{color: 'white'}}>Token</strong> is a string, like: <strong
-          style={{color: 'white'}}>110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw</strong> <br/>
-          which is required to authorize the bot and send requests to the Bot API. <br/>
-          Keep your token secure and store it safely, it can be used by anyone to control your bot. <br/>
+          The <strong style={{ color: 'white' }}>Token</strong> is a string, like: <strong
+          style={{ color: 'white' }}>110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw</strong> <br />
+          which is required to authorize the bot and send requests to the Bot API. <br />
+          Keep your token secure and store it safely, it can be used by anyone to control your bot. <br />
           If you haven't created a Telegram bot yet, click on this link <a
           href="https://core.telegram.org/bots/features#botfather" target="_blank" rel="noopener noreferrer"><strong>Telegram
-          Bot</strong></a>
-          and follow the instructions to create one.
-          <br/>
-          <br/>
-          The <strong style={{color: 'white'}}>Chat ID </strong> is a string, like: <strong
-          style={{color: 'white'}}>110201543</strong> <br/>
+          Bot</strong></a> and follow the instructions to create one.
+          <br />
+          <br />
+          The <strong style={{ color: 'white' }}>Chat ID </strong> is a string, like: <strong
+          style={{ color: 'white' }}>110201543</strong> <br />
           The Chat ID is automatically generated by Telegram.
-          Each type of chat (private, group, channel) receives a unique ID <br/>
-          when it is created or when the bot interacts with it for the first time. <br/>
-          This ID is used to identify and direct messages correctly through the Telegram API.<br/>
+          Each type of chat (private, group, channel) receives a unique ID <br />
+          when it is created or when the bot interacts with it for the first time. <br />
+          This ID is used to identify and direct messages correctly through the Telegram API.<br />
           Click on this link and follow the steps to get the chat ID: <a
           href="https://gist.github.com/nafiesl/4ad622f344cd1dc3bb1ecbe468ff9f8a#how-to-get-telegram-bot-chat-id"
           target="_blank" rel="noopener noreferrer">
           <strong>How to get Telegram Bot Chat ID</strong>
         </a>
-
         </div>
       </div>
     </form>
-  )
-    ;
+  );
 });
 
 export default StepTree;
